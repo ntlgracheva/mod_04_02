4aa08c4c6df473e14ac3a29ebb3c8ece708cc86a
Natalia Gracheva <ntlgracheva@gmail.com>
2023-04-17 19:00:29 +0200

    corrected

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-
 'use strict'
-const productName = prompt("Введите Наименование товара");
-const productAmount = + prompt("Введите Количество товара");;
-const productCategory = prompt("Введите Категорию товара");
-const productPrice = + prompt("Введите Цену товара");
+const productName = "Ноутбук";
+const productAmount = 5;
+const productCategory = "Персональные компьютеры";
+const productPrice = 500;
 
-console.log(typeof (productName));
-console.log(typeof (productAmount));
-console.log(typeof (productCategory));
-console.log(typeof (productPrice));
+console.log('Наименование товара: ', productName);
+console.log('Общая сумма товара: ', productAmount * productPrice, "$");
 
-console.log(`На складе ${productAmount} единицы товара "${productName}" на сумму ${productAmount * productPrice} деревянных`);
+{
+    const productName = prompt("Введите Наименование товара");
+    const productAmount = + prompt("Введите Количество товара");;
+    const productCategory = prompt("Введите Категорию товара");
+    const productPrice = + prompt("Введите Цену товара");
+
+    console.log(typeof (productName));
+    console.log(typeof (productAmount));
+    console.log(typeof (productCategory));
+    console.log(typeof (productPrice));
+
+    console.log(`На складе ${productAmount} единицы товара "${productName}" на сумму ${productAmount * productPrice} деревянных`);
+}
